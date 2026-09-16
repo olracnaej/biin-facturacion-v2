d699b224398ff9b205f0c0088bcf31c863c5f5e6
Jean Carlo Andrade Rojas <jcandro97@gmail.com>
2026-09-15 21:11:23 -0600

    Inicio BI-IN Facturación V2

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        BI-IN Facturación V2
+      </h1>
+    </main>
+  );
+}
