0bf38d687fd13c090474e999628123d394e804e4
Jean Carlo Andrade Rojas <jcandro97@gmail.com>
2026-09-16 01:51:12 -0600

    Conexion Prisma y MySQL funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-export default function Home() {
+import { prisma } from "../lib/prisma";
+
+export default async function Home() {
+  const usuarios = await prisma.usuario.findMany();
+
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        BI-IN Facturación V2
-      </h1>
+    <main>
+      <h1>BI-IN Facturación V2</h1>
+
+      <h2>Usuarios</h2>
+
+      {usuarios.map((usuario) => (
+        <div key={usuario.id}>
+          {usuario.correo}
+        </div>
+      ))}
     </main>
   );
 }
